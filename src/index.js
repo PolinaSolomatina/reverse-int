0f213ce8fbb87a430add7d708e3f9b61f25984e8
Polina Solomatina <polina.solomatina15@gmail.com>
2023-02-22 16:29:00 +0300

    task's done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+        number = -n;
+} else {
+    number = n;
 }
+        let string = String(number);
+        console.log(string);
+        let reverseStr = "";
+    
+        for (let i = 0; i < string.length; i += 1) {
+            console.log(string[i]);
+            reverseStr = `${string[i]}${reverseStr}`;
+        }
+        
+        return Number(reverseStr);
+}
+
+// 162 170 506 564 233 520 228 165 535
